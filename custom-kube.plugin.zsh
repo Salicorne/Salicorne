b5c1e7228b26761138c8a9fed46550f2c9c36ea1
Timothée Raboeuf <timothee.raboeuf@gmail.com>
2020-06-12 14:32:14 +0200

    Added stern

--- custom-kube.plugin.zsh
+++ custom-kube.plugin.zsh
@@ -1,14 +1,17 @@
 alias k='kubectl'
 alias kg='kubectl get'
 alias ka='kubectl apply'
-alias kl='kubectl logs'
+alias kl='kubectl logs --tail 100'
 alias ke='kubectl exec -it'
 alias kdesc='kubectl describe'
 alias kdel='kubectl delete'
 alias ktop='kubectl top'
 alias ktopp='kubectl top pods --all-namespaces'
-alias k8s-set-namespace='kubectl config set-context --current --namespace'
+alias kns='kubectl config set-context --current --namespace'
+alias kx='kubectl config use-context'
 alias dc='docker-compose'
+
+alias stern='stern --since 1m --template "{{.Namespace}}/{{color .PodColor .PodName}}/{{color .ContainerColor .ContainerName}} {{.Message}}" --all-namespaces'
 
 if [ $commands[kubectl] ]; then
   source <(kubectl completion zsh)
@@ -20,4 +23,8 @@ fi
 
 if [ $commands[helm] ]; then
   source <(helm completion zsh)
+fi
+
+if [ $commands[stern] ]; then
+  source <(stern --completion=zsh)
 fi
